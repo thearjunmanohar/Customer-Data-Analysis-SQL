6ec337a2bf4850b7e9377e4a4c06bd18ef793110
thearjunmanohar <thearjunmanohar@gmail.com>
2026-04-03 13:47:58 +0530

    Add files via upload

---
+++ Business Problems Customer Data.sql
@@ -0,0 +1,29 @@
+
+
+-- 18 Business Problems & Solutions.
+ 
+-- 1.  Retrieve all customer records.
+-- 2.  Count total number of customers.
+-- 3.  List unique payment methods.
+-- 4.  Find customers older than 50.
+-- 5.  Count number of male and female customers.
+-- 6.  Find average age of customers.
+-- 7.  Count customers per payment method.
+-- 8.  Find the most used payment method.
+-- 9.  Find average age by gender.
+-- 10. Find customers between age 20 and 30.
+-- 11. Find top 3 most common age groups.
+-- 12. Find gender distribution within each payment method.
+-- 13. Find customers whose age is above average.
+-- 14. Rank customers by age (Using Window Function).
+-- 15. Find percentage usage of each payment method.
+-- 16 Find total customers in each age group.
+-- 17. Top payment method per Age group ( CTE ).
+-- 18. Top Payment Method per Gender. ( Using Ranking ) 
+
+
+
+
+
+
+
